d1cea338a2c013539084a99cd5ad20e2efd672e6
stevencarlislewalker <swalker@angoss.com>
2017-05-24 22:29:00 -0400

    keyword tags

--- makeplan.R
+++ makeplan.R
@@ -5,20 +5,22 @@ random_meal <- function(){
 }
 
 random_plan <- function(number_of_meals) {
-    masterlist = readLines('masterlist')
-    recentlist = readLines('recentlist')
+    masterlist = list_meals(TRUE, FALSE)
+    recentlist = list_meals(TRUE, TRUE)
     sample(setdiff(masterlist, recentlist),
            number_of_meals, replace = FALSE)
 }
 
 is_valid_meal <- function(meal) {
-    masterlist = readLines('masterlist')
+    masterlist = drop_keywords(list_meals())
     recentlist = readLines('recentlist')
     meal %in% setdiff(masterlist, recentlist)
 }
 
-list_meals <- function() {
-    readLines('masterlist')
+list_meals <- function(drop_keywords = FALSE, just_recent = FALSE) {
+    l = readLines(ifelse(just_recent, 'recentlist', 'masterlist'))
+    if(drop_keywords) l = drop_keywords(l)
+    l
 }
 
 use_meals <- function(meals) {
@@ -32,6 +34,18 @@ clear_recentlist <- function() {
     file.create('recentlist')
 }
 
+suggested_list <- function(keywords, ...) {
+    l = lapply(keywords, agrep, x = list_meals(), value = TRUE, ...)
+    r = Reduce(union, l)
+    s = colSums(outer(unlist(l), r, '=='))
+    r = r[order(s, decreasing = TRUE)]
+    sapply(strsplit(r, ';'), '[', 1)
+}
+
+drop_keywords <- function(meals) {
+    sapply(strsplit(meals, ';'), '[', 1)
+}
+
 e = list2env(list(plan = c()))
 
 #'
@@ -41,13 +55,14 @@ e = list2env(list(plan = c()))
 #'
 
 propose <- function(proposal) {
+    l = list_meals(TRUE)
     if(missing(proposal)) {
         e$proposal <- random_meal()
     } else if(is_valid_meal(proposal)) {
         e$proposal <- proposal
     } else {
         cat('invalid proposal. perhaps you meant:\n')
-        print(list_meals()[order(adist(proposal, list_meals()))[1:5]])
+        print(agrep(proposal, l, ignore.case = TRUE, value = TRUE))
         stop()
     }
     cat('current proposal: ', e$proposal, '\n')
